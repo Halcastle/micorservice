3356512612a1def328bcb635912bf65e36cde226
PanWang <2606030692@qq.com>
2021-11-03 18:03:47 +0800

    更新

---
+++ user-eage-service/src/main/java/com/imooc/user/response/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.imooc.user.response;
+
+/**
+ * @ClassName LoginResponse
+ * @Description TODO
+ * @Author pwang6
+ * @Date 2021/11/3 15:57
+ * @Version 1.0
+ **/
+public class LoginResponse extends Response {
+    public LoginResponse(String code, String message) {
+        super(code, message);
+    }
+}
--- user-eage-service/src/main/java/com/imooc/user/response/Response.java
+++ user-eage-service/src/main/java/com/imooc/user/response/Response.java
@@ -11,13 +11,18 @@ import java.io.Serializable;
  **/
 public class Response implements Serializable {
 
-    public static final Response USERNAME_PASSWORD_INVALID = new Response("1001","username or password is invalid!");
+    public static final Response USERNAME_PASSWORD_INVALID = new Response("1001", "username or password is invalid!");
 
     private String code;
 
     private String message;
 
-    public Response(String code,String message){
+    public Response() {
+        this.code = "0";
+        this.message = "success";
+    }
+
+    public Response(String code, String message) {
         this.code = code;
         this.message = message;
     }
